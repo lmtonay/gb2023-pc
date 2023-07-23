b1f0db11e2d76e9e4d20d0a7cfbf71f9ec035ed6
Labib Mahfuz Tonay <lm.tonay1@gmail.com>
2023-07-23 23:17:18 +0600

    CM and Submissions added

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 "use strict";
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 var container = document.getElementById('certificates');
+var allNames = [].concat(_toConsumableArray(names), _toConsumableArray(names2));
+allNames.sort(function (a, b) {
+  return a.toLowerCase().localeCompare(b.toLowerCase());
+});
 var input = document.getElementById('search');
 function pushIntoDOM(array) {
   container.innerHTML = '';
@@ -19,11 +29,11 @@ function pushIntoDOM(array) {
 }
 input.addEventListener('keyup', function (e) {
   var value = e.target.value;
-  var filtered = names.filter(function (item) {
+  var filtered = allNames.filter(function (item) {
     return item.toLowerCase().includes(value.toLowerCase());
   });
   pushIntoDOM(filtered);
 });
 document.addEventListener('DOMContentLoaded', function () {
-  pushIntoDOM(names);
+  pushIntoDOM(allNames);
 });
